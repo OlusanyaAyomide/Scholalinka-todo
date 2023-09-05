3c3116542d8debd564e34cbd9181e819f42f4074
AYOMIDE <AYOMIDE>
2023-09-05 16:11:57 +0100

    deploy-bug-fix

--- todoClient/src/utils/requestUtils.ts
+++ todoClient/src/utils/requestUtils.ts
@@ -1,7 +1,7 @@
 import Cookies from "js-cookie"
 
 export function setCookieAsync(token:string){
-    return new Promise((resolve,reject)=>{
+    return new Promise((resolve)=>{
       Cookies.set('authCookie',token, { expires: 30 })
       resolve(null)
     })
